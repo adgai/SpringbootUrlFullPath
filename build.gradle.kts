2e55774cd2e81c5f4887de0260d85f574d61e3c3
handy <handyplus@163.com>
2026-08-10 10:37:45 +0800

    修复无参 Mapping URL 显示并升级至 3.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.adgainai"
-version = "2.0.1"
+version = "3.0.0"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/com/adgainai/springbooturlfullpath/JavaMappingUrlProvider.kt
+++ src/main/kotlin/com/adgainai/springbooturlfullpath/JavaMappingUrlProvider.kt
@@ -105,6 +105,14 @@ class JavaMappingUrlProvider : CodeVisionProvider<Unit> {
         if (classPaths.isEmpty()) {
             return methodPaths.map { it -> it.cleanPath() }.toCollection(ArrayList())
         }
+        if (methodPaths.isEmpty()) {
+            return classPaths.map { classPath ->
+                buildString {
+                    if (prefix.isNotBlank()) append(prefix)
+                    append(classPath.cleanPath())
+                }
+            }
+        }
         return buildList {
             classPaths.forEach { classPath ->
                 methodPaths.forEach { methodPath ->
